d9ad41d6f8b8ab764b614637078e98d8f2f3d907
David G Chaves <davidgchaves@gmail.com>
2014-10-27 20:54:13 +0100

    Exercise 02-4: Reimplement 'last'

--- 02-first-steps.hs
+++ 02-first-steps.hs
@@ -5,3 +5,15 @@ n = a `div` length xs
     where a = 10
           xs = [1,2,3,4,5]
 
+--
+-- Exercise 4: Reimplement 'last'
+--
+ns = [1,2,3,4,5]
+cs = "aeiou"
+
+last'   xs = head (drop (length xs - 1) xs)
+last''  xs = head (reverse xs)
+last''' xs = xs !! (length xs - 1)
+-- last ns --> 5
+-- last cs --> 'u'
+
