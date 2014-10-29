bce2cba26933ba698228ef262e6ab8bee3ef5d17
David G Chaves <davidgchaves@gmail.com>
2014-10-29 15:25:29 +0100

    Exercise 03-2: Guess the types of the following functions

--- 03-types-and-classes.hs
+++ 03-types-and-classes.hs
@@ -16,3 +16,25 @@ v4 :: ([Bool], [Char])
 v5 = [tail, init, reverse]
 v5 :: [ [a] -> [a] ]
 
+
+--
+-- Exercise 2: What are the types of the following functions?
+--
+second :: [a] -> a
+second xs = head (tail xs)
+
+swap :: (a, b) -> (b, a)
+swap (x, y) = (y, x)
+
+pair :: a -> b -> (a, b)
+pair x y = (x, y)
+
+double :: Num a => a -> a
+double x = x * 2
+
+palindrome :: Eq a => [a] -> Bool
+palindrome xs = reverse xs == xs
+
+twice :: (a -> a) -> a -> a
+twice f x = f (f x)
+
