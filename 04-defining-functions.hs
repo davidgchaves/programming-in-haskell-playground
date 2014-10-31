835893add00080c4e6720d3521d40f4326391238
David G Chaves <davidgchaves@gmail.com>
2014-10-31 13:48:17 +0100

    Exercise 04-3: Logical disjunction operator (||) implementations

--- 04-defining-functions.hs
+++ 04-defining-functions.hs
@@ -46,3 +46,35 @@ safetail''''
             []     -> []
             (_:xs) -> xs
 
+
+--
+-- Exercise 3: Logical disjunction operator (|| i.e. OR) implementations
+--
+(|||) :: Bool -> Bool -> Bool
+False ||| False = False
+_     ||| _     = True
+
+(||||) :: Bool -> Bool -> Bool
+False |||| b = b
+True  |||| _ = True
+
+(|||||) :: Bool -> Bool -> Bool
+b ||||| c
+    | b == c    = b
+    | otherwise = True
+
+(||||||) :: Bool -> Bool -> Bool
+b |||||| False = b
+_ |||||| True  = True
+
+(|||||||) :: Bool -> Bool -> Bool
+b ||||||| c
+    | b == c    = c
+    | otherwise = True
+
+(||||||||) :: Bool -> Bool -> Bool
+False |||||||| False = False
+False |||||||| True  = True
+True  |||||||| False = True
+True  |||||||| True  = True
+
