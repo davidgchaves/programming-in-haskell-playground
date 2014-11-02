4ebe8db71cfe179b4ba67c6f33d65b27423e6687
David G Chaves <davidgchaves@gmail.com>
2014-11-02 13:36:22 +0100

    Caesar Cipher: int2LowerChar

--- 05-the-caesar-cipher.hs
+++ 05-the-caesar-cipher.hs
@@ -15,3 +15,10 @@ lowerChar2Int c = ord c - ord 'a'
 -- lowerChar2Int 'a' --> 0
 -- lowerChar2Int 'z' --> 25
 
+-- converts an integer between 0 and 25
+-- into the corresponding lower-case letter between 'a' and 'z'
+int2LowerChar :: Int -> Char
+int2LowerChar n = chr (n + ord 'a')
+-- int2LowerChar 0  --> 'a'
+-- int2LowerChar 25 --> 'z'
+
