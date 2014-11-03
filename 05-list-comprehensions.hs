4c6752a9f077de3ba3400aecc4e044ac07bce49e
David G Chaves <davidgchaves@gmail.com>
2014-11-03 14:55:47 +0100

    Exercise 05-2: Reimplement 'replicate' using a list comprehension

--- 05-list-comprehensions.hs
+++ 05-list-comprehensions.hs
@@ -9,3 +9,13 @@ sumSquares' :: Int -> Int
 sumSquares' n = foldl (+) (0) [ x^2 | x <- [1..n] ]
 -- sumSquares' 100 --> 338350
 
+
+--
+-- Exercise 2: Reimplement 'replicate' using a list comprehension
+--             replicate :: Int -> a -> [a] produces a list of identical elements
+--             replicate 3 True --> [True, True, True]
+--
+replicate' :: Int -> a -> [a]
+replicate' n a = [ a | _ <- [1..n] ]
+-- replicate' 8 "NaN" --> ["NaN","NaN","NaN","NaN","NaN","NaN","NaN","NaN"]
+
