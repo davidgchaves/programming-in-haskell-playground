a0bb3f8f400979314c857c53dfef0e0dbb0bd47c
David G Chaves <davidgchaves@gmail.com>
2014-11-03 15:07:48 +0100

    Exercise 05-3: Implement 'pyths' using a list comprehension
    'pyths' produces the list of all pythagorean triples (x^2 + y^2 = z^2)
    whose components are at most a given limit n

--- 05-list-comprehensions.hs
+++ 05-list-comprehensions.hs
@@ -19,3 +19,14 @@ replicate' :: Int -> a -> [a]
 replicate' n a = [ a | _ <- [1..n] ]
 -- replicate' 8 "NaN" --> ["NaN","NaN","NaN","NaN","NaN","NaN","NaN","NaN"]
 
+
+--
+-- Exercise 3: Implement 'pyths' using a list comprehension
+--             'pyths' produces the list of all pythagorean triples (x^2 + y^2 = z^2)
+--                     whose components are at most a given limit n
+--             pyths 10 --> [(3, 4, 5), (4, 3, 5), (6, 8, 10), (8, 6, 10)]
+--
+pyths :: Int -> [(Int, Int, Int)]
+pyths n = [ (x,y,z) | x <- [1..n], y <- [1..n], z <- [1..n], x^2 + y^2 == z^2 ]
+-- pyths 13 --> [(3,4,5), (4,3,5), (5,12,13), (6,8,10), (8,6,10), (12,5,13)]
+
