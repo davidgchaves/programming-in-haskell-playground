182e9d879469b5c65c91322b6a803b8f71f10d25
David G Chaves <davidgchaves@gmail.com>
2014-11-03 18:41:20 +0100

    Exercise 05-8: Caesar cipher to also handle upper-case letters

--- 05-list-comprehensions.hs
+++ 05-list-comprehensions.hs
@@ -1,3 +1,4 @@
+import Data.Char -- for Exercise 8
 --
 -- Exercise 1: Expression that calculates the sum 1^2 + 2^2 + ... 100^2
 --
@@ -78,4 +79,55 @@ positions x xs = find x (zip xs [0..n])
 scalarProduct :: Num a => [a] -> [a] -> a
 scalarProduct xs ys = sum [ x*y | (x,y) <- zip xs ys ]
 -- scalarProduct [1,2,3] [4,5,6] --> 32
+
+
+--
+-- Exercise 8: Modify the Caesar cipher program to also handle upper-case letters
+--
+
+-- converts a lower-case letter between 'a' and 'z'
+-- into the corresponding integer between 0 and 25
+lowerChar2Int :: Char -> Int
+lowerChar2Int c = ord c - ord 'a'
+-- lowerChar2Int 'a' --> 0
+-- lowerChar2Int 'z' --> 25
+
+-- converts an upper-case letter between 'A' and 'Z'
+-- into the corresponding integer between 0 and 25
+upperChar2Int :: Char -> Int
+upperChar2Int c = ord c - ord 'A'
+-- upperChar2Int 'A' --> 0
+-- upperChar2Int 'Z' --> 25
+
+-- converts an integer between 0 and 25
+-- into the corresponding lower-case letter between 'a' and 'z'
+int2LowerChar :: Int -> Char
+int2LowerChar n = chr (n + ord 'a')
+-- int2LowerChar 0  --> 'a'
+-- int2LowerChar 25 --> 'z'
+
+-- converts an integer between 0 and 25
+-- into the corresponding upper-case letter between 'A' and 'Z'
+int2UpperChar :: Int -> Char
+int2UpperChar n = chr (n + ord 'A')
+-- int2UpperChar 0  --> 'A'
+-- int2UpperChar 25 --> 'Z'
+
+-- applies a shift factor n to a letter c
+shiftChar :: Int -> Char -> Char
+shiftChar n c
+    | isLower c = int2LowerChar ((lowerChar2Int c + n) `mod` 26)
+    | isUpper c = int2UpperChar ((upperChar2Int c + n) `mod` 26)
+    | otherwise = c
+-- shiftChar 2 'a' --> 'c'
+-- shiftChar 1 'z' --> 'a'
+-- shiftChar 2 'A' --> 'C'
+-- shiftChar 1 'Z' --> 'A'
+-- shiftChar 5 '?' --> '?'
+
+-- encodes a string using a given shift factor
+encode :: Int -> String -> String
+encode n xs = [ shiftChar n x | x <- xs ]
+-- encode 13 "Think like a Fundamentalist Code like a Hacker"
+--       --> "Guvax yvxr n Shaqnzragnyvfg Pbqr yvxr n Unpxre"
 
