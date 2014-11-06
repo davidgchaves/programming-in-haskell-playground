1e1840ea44eb60edf1b64983343ff5508b68549d
David G Chaves <davidgchaves@gmail.com>
2014-11-06 18:29:48 +0100

    Recursion: product

--- 06-recursion.hs
+++ 06-recursion.hs
@@ -9,3 +9,10 @@ m `prod` 0 = 0
 m `prod` n = m + (m `prod` (n-1))
 -- 5 `prod` 4 --> 20
 
+
+-- 6.2 Recursion on Lists
+product' :: Num a => [a] -> a
+product' []     = 1
+product' (n:ns) = n * product' ns
+-- product' [2,4,6] --> 48
+
