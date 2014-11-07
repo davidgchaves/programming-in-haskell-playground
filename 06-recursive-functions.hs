c89d74d23f51c368a957ddf1873ec088b92565fe
David G Chaves <davidgchaves@gmail.com>
2014-11-07 17:40:06 +0100

    Exercise 06-2: Reimplement and

--- 06-recursive-functions.hs
+++ 06-recursive-functions.hs
@@ -11,3 +11,25 @@ m ^^^^ 0 = 1
 m ^^^^ n = m * (^^^^) m (n-1)
 -- 3 ^^^^ 4 --> 81
 
+
+--
+-- Exercise 2: Reimplement and
+--
+and' :: [Bool] -> Bool
+and' []     = True
+and' (b:bs) = b && and' bs
+
+and'' :: [Bool] -> Bool
+and'' []                 = True
+and'' (b:bs) | b         = and'' bs
+             | otherwise = False
+
+and''' :: [Bool] -> Bool
+and''' []                  = True
+and''' (b:bs) | b == False = False
+              | otherwise  = and''' bs
+
+and'''' :: [Bool] -> Bool
+and'''' []     = True
+and'''' (b:bs) = and'''' bs && b
+
