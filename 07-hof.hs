c7c53fac7db958d5d86801cf3a04cd00708ad30b
David G Chaves <davidgchaves@gmail.com>
2014-11-11 12:01:24 +0100

    HoF: map defined using recursion

--- 07-hof.hs
+++ 07-hof.hs
@@ -5,3 +5,9 @@ map'      :: (a -> b) -> [a] -> [b]
 map' f xs = [f x | x <- xs]
 -- map' (+1) [1,3,5,7,9] --> [2,4,6,8,10]
 
+-- map defined using recursion
+map''          :: (a -> b) -> [a] -> [b]
+map'' _ []     = []
+map'' f (x:xs) = f x : map'' f xs
+-- map'' (+1) [1,3,5,7,9] --> [2,4,6,8,10]
+
