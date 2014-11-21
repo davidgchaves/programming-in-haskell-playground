6d34538b50151c6610db53a282794d15cb26bca8
David G Chaves <davidgchaves@gmail.com>
2014-11-21 08:32:47 +0100

    Exercise 07-3: any (for finite, non-partial input lists) using HoFs

--- 07-higher-order-functions.hs
+++ 07-higher-order-functions.hs
@@ -73,3 +73,71 @@ all7 p = foldr (&&) True . map p
 -- all7 even [1..10]   --> False
 -- all7 even []        --> True
 
+
+--
+-- Exercise 3: Reimplement any (for finite, non-partial input lists) using HoFs
+--
+
+-- using or and map
+any1      :: (a -> Bool) -> [a] -> Bool
+any1 p xs = or (map p xs)
+-- any1 even []        --> False
+-- any1 even [1..10]   --> True
+-- any1 even [1,3,5,7] --> False
+
+-- composing or and map
+any2   :: (a -> Bool) -> [a] -> Bool
+any2 p = or . map p
+-- any2 even []        --> False
+-- any2 even [1..10]   --> True
+-- any2 even [1,3,5,7] --> False
+
+-- using filter
+any3      :: (a -> Bool) -> [a] -> Bool
+any3 p xs = length (filter p xs) > 0
+-- any3 even []        --> False
+-- any3 even [1..10]   --> True
+-- any3 even [1,3,5,7] --> False
+
+-- Absurd
+any4   :: (a -> Bool) -> [a] -> Bool
+any4 p = not . null . dropWhile (not . p)
+-- any4 even []        --> False
+-- any4 even [1..10]   --> True
+-- any4 even [1,3,5,7] --> False
+
+-- composing not, null and filter
+any5   :: (a -> Bool) -> [a] -> Bool
+any5 p = not . null . filter p
+-- any5 even []        --> False
+-- any5 even [1..10]   --> True
+-- any5 even [1,3,5,7] --> False
+
+-- convolution FTW!
+any6      :: (a -> Bool) -> [a] -> Bool
+any6 p xs = not (all (\x -> not (p x)) xs)
+-- any6 even []        --> False
+-- any6 even [1..10]   --> True
+-- any6 even [1,3,5,7] --> False
+
+-- convolution FTW! (v2) (easier to use || instead the lambda)
+any7      :: (a -> Bool) -> [a] -> Bool
+any7 p xs = foldr (\x acc -> (p x) || acc) False xs
+-- any7 even []        --> False
+-- any7 even [1..10]   --> True
+-- any7 even [1,3,5,7] --> False
+
+-- using foldr (like any7 but revealing intent)
+any8      :: (a -> Bool) -> [a] -> Bool
+any8 p xs = foldr (||) False (map p xs)
+-- any8 even []        --> False
+-- any8 even [1..10]   --> True
+-- any8 even [1,3,5,7] --> False
+
+-- composing foldr (like any7 but revealing intent)
+any9   :: (a -> Bool) -> [a] -> Bool
+any9 p = foldr (||) False . map p
+-- any9 even []        --> False
+-- any9 even [1..10]   --> True
+-- any9 even [1,3,5,7] --> False
+
