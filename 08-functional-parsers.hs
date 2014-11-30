a36e292b17213157ff445538b5d96ff669dd5a19
David G Chaves <davidgchaves@gmail.com>
2014-11-30 08:16:19 +0100

    Functional Parsers (FuncPa): Introduction

---
+++ 08-functional-parsers.hs
@@ -0,0 +1,6 @@
+-- Parsers:
+--  * A parser is a program that
+--      - takes a string of characters, and
+--      - produces some form of tree (making the syntactic structure of the string explicit)
+--  * Most real-life programs use a parser to pre-process their input
+
