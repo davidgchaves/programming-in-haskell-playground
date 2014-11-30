a55503545037bd6e4ea6f2736147bcbaa3fe17e6
David G Chaves <davidgchaves@gmail.com>
2014-11-30 08:47:28 +0100

    FuncPa: Evolution of the Parser type

--- 08-functional-parsers.hs
+++ 08-functional-parsers.hs
@@ -4,3 +4,23 @@
 --      - produces some form of tree (making the syntactic structure of the string explicit)
 --  * Most real-life programs use a parser to pre-process their input
 
+-- Evolution of the Parser type
+--  * The notion of a parser can be represented as a function of
+--      type Parser = String -> Tree
+--
+--  * A parser might not always consume its entire argument string,
+--    (return any unconsumed part of the argument string):
+--      type Parser = String -> (Tree, String)
+--
+--  * A parser might not always succeed or might have multiple results:
+--      - The empty list            denotes failure
+--      - A singleton list          denotes success
+--      - More than one pair list   denotes ambiguous grammar
+--        (ambiguous grammar: the argument string can be parsed in more than one way)
+--      NOTE: For simplicity we only consider parsers that return at most one result
+--      type Parser = String -> [(Tree, String)]
+--
+--  * Different parsers will likely return different kinds of trees,
+--    or more generally, any kind of value a (parameter of the Parser type)
+type Parser a = String -> [(a, String)]
+
