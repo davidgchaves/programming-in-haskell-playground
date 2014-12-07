c90f9a33f4e1f6e65200390932b0f80c4bda1f75
David G Chaves <davidgchaves@gmail.com>
2014-12-07 16:21:41 +0100

    IntProgs (interactive programs): getChar, putChar c, return v

---
+++ 09-interactive-programs.hs
@@ -0,0 +1,14 @@
+-- 9.3 Basic actions
+
+--  (1) getChar :: IO Char
+--      'getChar' reads a character from the keyboard,
+--      echoes it to the screen,
+--      returns the character as its result value
+--
+--  (2) putChar :: Char -> IO ()
+--      'putChar c' writes the character c to the screen,
+--      returns no result value, represented by the empty tuple () (aka Unit)
+--
+--  (3) return  :: a -> IO ()
+--      'return v' returns v without performing any interaction
+
