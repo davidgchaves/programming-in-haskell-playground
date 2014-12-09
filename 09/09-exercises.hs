1a33c94950c7873c944b088417427d25f21e0475
David G Chaves <davidgchaves@gmail.com>
2014-12-09 10:45:35 +0100

    Extra Exercise 09-2: Implement putStrLn

--- 09/09-exercises.hs
+++ 09/09-exercises.hs
@@ -9,3 +9,24 @@ putStr'        :: String -> IO ()
 putStr' []     = return ()
 putStr' (x:xs) = putChar x >> putStr' xs
 
+
+-- Extra Exercise 09-2:
+--  Define a function putStrLn :: String -> IO () that
+--      - takes a String and
+--      - writes it to the standard output, followed by a newline character
+putStrLn'    :: String -> IO ()
+putStrLn' [] = putChar '\n'
+putStrLn' xs = putStr' xs >> putStrLn' ""
+
+putStrLn''    :: String -> IO ()
+putStrLn'' [] = putChar '\n'
+putStrLn'' xs = putStr' xs >> putChar '\n'
+
+putStrLn'''    :: String -> IO ()
+putStrLn''' [] = putChar '\n'
+putStrLn''' xs = putStr' xs >>= \x -> putChar '\n'
+
+putStrLn''''    :: String -> IO ()
+putStrLn'''' [] = putChar '\n'
+putStrLn'''' xs = putStr' xs >> putStr' "\n"
+
