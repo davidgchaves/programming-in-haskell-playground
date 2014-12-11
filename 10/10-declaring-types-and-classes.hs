77a67872f1ab2e62940bf2e4bc9b893c27fa853b
David G Chaves <davidgchaves@gmail.com>
2014-12-11 09:20:36 +0100

    Declaring Types and Classes (DTaC): Type declarations

---
+++ 10/10-declaring-types-and-classes.hs
@@ -0,0 +1,7 @@
+-- 10.1 Type declarations
+
+--  TYPE DECLARATIONS: A new name (SYNONYM) for an existing type
+--      - The name of a new type must began with CAPITAL LETTER
+--      - Cannot be recursive
+--      - Can be parameterised (with more than one type parameter) by other types
+
