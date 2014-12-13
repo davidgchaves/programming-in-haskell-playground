50865a29fde6bba9220e7c86df5ca68b141425ac
David G Chaves <davidgchaves@gmail.com>
2014-12-13 09:32:44 +0100

    DTaC: Tree example, does an int occurs in a binary tree?

--- 10/10-declaring-types-and-classes.hs
+++ 10/10-declaring-types-and-classes.hs
@@ -189,3 +189,10 @@ exampleTree = Node (Node (Leaf 1) 3 (Leaf 4))
                    5
                    (Node (Leaf 6) 7 (Leaf 9))
 
+-- occurs: decides if a given integer occurs in a tree
+occurs                :: Int -> Tree -> Bool
+occurs m (Leaf n)     = m == n
+occurs m (Node l n r) = m == n || occurs m l || occurs m r
+-- occurs 8 exampleTree --> False
+-- occurs 4 exampleTree --> True
+
