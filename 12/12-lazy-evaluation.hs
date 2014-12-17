82c3ede998cd8285f1b38764d689c31b8f654355
David G Chaves <davidgchaves@gmail.com>
2014-12-17 08:45:28 +0100

    LazyE: Evaluation strategies (innermost and outermost)

--- 12/12-lazy-evaluation.hs
+++ 12/12-lazy-evaluation.hs
@@ -11,3 +11,35 @@
 -- In Haskell, any two different ways of evaluating the same expression
 -- will always produce the same final value, provided that they both terminate
 
+
+-- 12.2 Evaluation strategies
+
+-- At each stage during evaluation of an expression there may be
+-- many possible subexpressions that can be reduced by applying a definition
+
+-- RedEx (Reducible Expression):
+--  An expression that has the form of a function applied to one or more arguments
+--  (and can be "reduced" by performing the function application)
+
+-- Innermost Evaluation (An Innermost RedEx is always reduced):
+--  - Always choose a RedEx that is innermost (it contains no other RedEx),
+--    from left to right, in case there's more than one innermost RedEx
+--  - Ensures that the argument of a function is always fully evaluated
+--    before the function itself is applied => Arguments are passed BY VALUE
+--  - Using Innermost Evaluation is normally refered to as CALL-BY-VALUE Evaluation
+
+-- Outermost Evaluation (An Outermost RedEx is always reduced):
+--  - Always choose a RedEx that is outermost (it is contained in no other RedEx),
+--    from left to right, in case there's more than one outermost RedEx
+--  - Allows functions to be applied before their arguments are evaluated
+--    => Arguments are passed BY NAME
+--  - Using Outermost Evaluation is normally refered to as CALL-BY-NAME Evaluation
+
+-- Reductions and Lambda Expressions:
+--  - Functions are viewed as black boxes that we're not permitted to look inside
+--    => The selection of RedExes within Lambda Expressions is prohibited
+--  - The only operation that can be performed on a function is that
+--    of applying it to an argument
+--  - Reductions within the body of a function is only permitted
+--    once the function has been applied
+
